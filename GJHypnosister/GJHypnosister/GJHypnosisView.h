ffc84aa2e2bb537d6e2e2f0e09c21e082e136bf7
guojian2080 <guojian2080@163.com>
2016-05-08 22:46:06 +0800

    iOS Prov4 C4 primary practise

---
+++ GJHypnosister/GJHypnosister/GJHypnosisView.h
@@ -0,0 +1,13 @@
+//
+//  GJHypnosisView.h
+//  GJHypnosister
+//
+//  Created by 郭健 on 16/5/4.
+//  Copyright © 2016年 gj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GJHypnosisView : UIView
+
+@end
