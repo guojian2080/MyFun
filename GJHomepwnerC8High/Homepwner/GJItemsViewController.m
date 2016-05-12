36786a9d509f4b21b4e2946a8ed78a2e27e9b68d
guojian2080 <guojian2080@163.com>
2016-05-12 21:29:00 +0800

    iOS Prov4 C8 high practise

---
+++ GJHomepwnerC8High/Homepwner/GJItemsViewController.m
@@ -0,0 +1,71 @@
+//
+//  GJItemsViewController.m
+//  Homepwner
+//
+//  Created by 郭健 on 16/5/10.
+//  Copyright © 2016年 gj. All rights reserved.
+//
+
+#import "GJItemsViewController.h"
+#import "GJItemStore.h"
+#import "GJItem.h"
+@implementation GJItemsViewController
+
+- (instancetype) init
+{
+    self = [super initWithStyle:UITableViewStylePlain];
+    if (self) {
+        for (int i = 0; i < 5; i++) {
+            [[GJItemStore sharedStore] createItem];
+        }
+    }
+    return  self;
+}
+
+- (instancetype) initWithStyle:(UITableViewStyle)style
+{
+    return [self init];
+}
+
+- (void) viewDidLoad
+{
+    [super viewDidLoad];
+    
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"UITableViewCell"];
+    
+    UIImageView *bgView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"bg"]];
+    self.tableView.backgroundView = bgView;
+}
+
+#pragma mark - dataSource方法
+- (NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [[[GJItemStore sharedStore] allItems] count] + 1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"UITableViewCell" forIndexPath:indexPath];
+    
+    cell.backgroundColor = [UIColor clearColor];
+    NSArray *items = [[GJItemStore sharedStore] allItems];
+    if (indexPath.row < [items count]) {
+        GJItem *item = items[indexPath.row];
+        cell.textLabel.text = [item description];
+    }else{
+        cell.textLabel.text = @"No more items!";
+    }
+    return cell;
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSArray *items = [[GJItemStore sharedStore] allItems];
+    if (indexPath.row < [items count]) {
+        return 60;
+    }else{
+        return 44;
+    }
+}
+
+@end
