551431464ed266b2e4fcf3e49833978e0fd7a5ed
guojian2080 <guojian2080@163.com>
2016-05-14 23:18:30 +0800

    iOS Prov4 C10 primary practise

---
+++ GJHomepwnerC10Primary/Homepwner/GJDetailViewController.m
@@ -0,0 +1,57 @@
+//
+//  GJDetailViewController.m
+//  Homepwner
+//
+//  Created by 郭健 on 16/5/14.
+//  Copyright © 2016年 gj. All rights reserved.
+//
+
+#import "GJDetailViewController.h"
+#import "GJItem.h"
+@interface GJDetailViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *nameField;
+@property (weak, nonatomic) IBOutlet UITextField *serialNumberField;
+@property (weak, nonatomic) IBOutlet UITextField *valueField;
+@property (weak, nonatomic) IBOutlet UILabel *dateLabel;
+
+@end
+
+@implementation GJDetailViewController
+
+- (void) setItem:(GJItem *)item
+{
+    _item = item;
+    self.navigationItem.title = _item.itemName;
+}
+
+- (void) viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    GJItem *item = self.item;
+    
+    self.nameField.text = item.itemName;
+    self.serialNumberField.text = item.serialNumber;
+    self.valueField.text = [NSString stringWithFormat:@"%d",item.valueInDollars];
+    
+    static NSDateFormatter *dateFormatter = nil;
+    if(!dateFormatter){
+        dateFormatter = [[NSDateFormatter alloc] init];
+        dateFormatter.dateStyle = NSDateFormatterMediumStyle;
+        dateFormatter.timeStyle = NSDateFormatterNoStyle;
+    }
+    self.dateLabel.text = [dateFormatter stringFromDate:item.dateCreated];
+}
+
+- (void) viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    
+    [self.view endEditing:YES];
+    
+    GJItem *item = self.item;
+    item.itemName = self.nameField.text;
+    item.serialNumber = self.serialNumberField.text;
+    item.valueInDollars = [self.valueField.text intValue];
+}
+
+@end
