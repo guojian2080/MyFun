a5a6f8bebe9eb6575c17f6c0ee9e15f5558c3e73
guojian2080 <guojian2080@163.com>
2016-05-16 23:41:31 +0800

    iOS Prov4 C12 primary practise

---
+++ GJTouchTrackerC12Primary/GJTouchTracker/GJAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GJTouchTracker
+//
+//  Created by 郭健 on 16/5/16.
+//  Copyright © 2016年 gj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GJTouchTrackerC12Primary/GJTouchTracker/GJDrawView.h
@@ -0,0 +1,13 @@
+//
+//  GJDrawView.h
+//  GJTouchTracker
+//
+//  Created by 郭健 on 16/5/16.
+//  Copyright © 2016年 gj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GJDrawView : UIView
+
+@end
