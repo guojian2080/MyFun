cc415c42815867f80756a14e3eadbeb8abbbaf0a
guojian2080 <guojian2080@163.com>
2016-05-29 15:40:43 +0800

    iOS Prov4 C15 middle practise

---
+++ GJQuiz15Middle/GJQuiz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GJQuiz
+//
+//  Created by 郭健 on 16/1/7.
+//  Copyright © 2016年 gj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
