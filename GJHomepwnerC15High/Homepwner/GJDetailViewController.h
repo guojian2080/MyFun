3a1c17a318d4f612b843d27a31579ab286072d8c
guojian2080 <guojian2080@163.com>
2016-05-29 19:17:50 +0800

    iOS Prov4 C15 high practise

---
+++ GJHomepwnerC15High/Homepwner/GJDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  GJDetailViewController.h
+//  Homepwner
+//
+//  Created by 郭健 on 16/5/14.
+//  Copyright © 2016年 gj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@class GJItem;
+@interface GJDetailViewController : UIViewController
+@property (nonatomic, strong) GJItem *item;
+@end
