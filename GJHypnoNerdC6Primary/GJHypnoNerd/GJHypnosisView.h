0b33b03f88c648e1f48bcafebb40a0186f839d62
guojian2080 <guojian2080@163.com>
2016-08-21 23:45:43 +0800

    iOS Prov4 C6 primary practise-update name

---
+++ GJHypnoNerdC6Primary/GJHypnoNerd/GJHypnosisView.h
@@ -0,0 +1,15 @@
+//
+//  GJHypnosisView.h
+//  GJHypnosister
+//
+//  Created by 郭健 on 16/5/4.
+//  Copyright © 2016年 gj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GJHypnosisView : UIView
+
+@property (nonatomic, strong) UIColor *circleColor;
+
+@end
